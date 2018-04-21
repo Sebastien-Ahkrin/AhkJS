227fde391be7bc689ae1223f3cfde42afb3d30bd
Sebastien <sebastien48criquet@gmail.com>
2018-04-21 19:25:25 +0200

    use ahkframework

--- commands/index.js
+++ commands/index.js
@@ -1,10 +1,62 @@
-const add = require("./Add.js")
-const ban = require("./Ban.js")
-const clr = require("./Clr.js")
-const git = require("./GitHub.js")
-const ping = require("./Ping.js")
-const help = require("./Help.js")
+const clr = {
+    name: 'clear',
+    description: 'Clear a channel',
+    examples: ['clear ["number"]'],
+    aliases: ['clr'],
+    permissions: {
+        user: ['MANAGE_MESSAGES'],
+        client: ['MANAGE_MESSAGES']
+    }
+}
 
-module.exports = [
-    add, ban, clr, git, ping, help
-]
+const add = {
+    name: 'add',
+    description: 'Send a message for add Ahk in your guilde.',
+    examples: ['add'],
+    aliases: ['add'],
+    permissions: {
+        user: ['SEND_MESSAGES'],
+        client: ['SEND_MESSAGES']
+    }
+}
+
+const ban = {
+    name: 'ban',
+    description: 'Ban a user [Only for Admin]',
+    examples: ['ban @Ahkrin#0001'],
+    aliases: ['ban'],
+    permissions: {
+        user: ['BAN_MEMBERS'],
+        client: ['BAN_MEMBERS']
+    }
+}
+
+const github = {
+    name: 'github',
+    description: 'Show My GitHub Account.',
+    examples: ['github'],
+    aliases: ['git'],
+    permissions: {
+        user: ['SEND_MESSAGES'],
+        client: ['SEND_MESSAGES']
+    }
+}
+
+const ping = {
+    name: 'ping',
+    description: 'pong',
+    examples: ['ping'],
+    aliases: ['ping'],
+    permissions: {
+        user: ['SEND_MESSAGES'],
+        client: ['SEND_MESSAGES']
+    }
+}
+
+module.exports = {
+    clr,
+    add,
+    ban,
+    github,
+    ping
+}
--- index.js
+++ index.js
@@ -1,33 +1,121 @@
 require('dotenv').load()
 
-const commands = require("./commands")
-const Bot = require("./Bot.js")
+const Discord = require('discord.js');
 
-const config = {
-    discord: process.env.DISCORD_TOKEN,
-    twitch: process.env.TWITCH_TOKEN,
-    servers: [
-        {
-            id: process.env.GALHARIM_ID,
-            channel: process.env.GALHARIM_CHAN_ID,
-            message: {
-                color: "#EFEA6B",
-                fields: [
-                    {
-                        title: "Bienvenue à %name%",
-                        content: "Souhaitez la bienvenue au nouveau vous autres, ou soyez maudit !"
-                    }
-                ]
-            }
-        }
-    ],
-    bot: {
-        prefix: "$",
-        name: "Ahk",
-        game: "help",
-        icon_path: "./ressources/Ahk.png"
-    },
-    commands: commands
+const commands = require('./commands')
+
+const Bot = require('ahkframework')
+const prefix = '?'
+
+const galharim = {
+    id: process.env.GALHARIM_ID,
+    channel: process.env.GALHARIM_CHAN_ID
 }
 
-new Bot(config);
+const config = {
+    token: process.env.DISCORD_TOKEN,
+    name: "Ahk",
+    prefix: prefix,
+    version: "1.0.0",
+    icon_path: "./ressource/Ahk.png",
+    presence: { name: prefix + "help" }
+}
+
+const user = new Bot(config)
+
+user.registerEvent('guildMemberAdd', (member) => {
+    if(member.guild.id === ""){
+        const embed = new Discord.RichEmbed()
+            .setAuthor(member.user.username, member.user.avatarURL)
+            .setColor('#EFEA6B')
+            .addField('Bienvenue à ' + member.user.username, 'Souhaitez la bienvenue au nouveau vous autres, ou soyez maudit !')
+
+        member.guild.channels.filter(channel => channel.id === "")
+            .forEach(chann => chann.send({ embed }))
+
+    }
+})
+
+user.registerCommand(commands.github, (message, channel, client, args) => {
+    const embed = new Discord.RichEmbed();
+        .setColor("#016AC7");
+        .setThumbnail("http://litarvan.github.io/krobot_icons/info_v2.png");
+        .addField("**" + "ping" + "**", client.ping.toFixed(0) + "ms.");
+    channel.send({ embed });
+})
+
+user.registerCommand(commands.github, (message, channel, client, args) => {
+    const embed = new Discord.RichEmbed()
+        .setColor("#016AC7")
+        .setThumbnail("http://litarvan.github.io/krobot_icons/info_v2.png")
+        .addField("**GitHub**", "https://github.com/Sebastien-Ahkrin/AhkJS")
+    channel.send({ embed })
+})
+
+
+user.registerCommand(commands.clr, (message, channel, client, args) => {
+    const number = args[0]
+    if(number <= 0 || number > 100) {
+        const embed = new Discord.RichEmbed()
+            .setColor("#EFEA6B")
+            .setThumbnail("http://litarvan.github.io/krobot_icons/warn.png")
+            .addField("**Error**", "L'argument doit être compris entre 1 et 100.")
+        channel.send({ embed })
+    }
+
+    const d = (number < 100 ? (parseInt(number) + 1) : number)
+    message.channel.fetchMessages({ limit: d }).then(list => {
+        message.channel.bulkDelete(list)
+    }).catch(error => {
+        const embed = new Discord.RichEmbed()
+            .setColor("#EFEA6B")
+            .setThumbnail("http://litarvan.github.io/krobot_icons/warn.png")
+            .addField("**Error**", "Une erreur est survenue.")
+        channel.send({ embed })
+    })
+
+})
+
+user.registerCommand(commands.add, (message, channel, client, args) => {
+    const embed = new Discord.RichEmbed()
+        .setColor('#016AC7')
+        .setThumbnail("http://litarvan.github.io/krobot_icons/info_v2.png")
+        .addField("**Message for add AhkJS**", "https://discordapp.com/oauth2/authorize?client_id=" + message.client.user.id + "&scope=bot")
+    channel.send({ embed })
+})
+
+user.registerCommand(commands.ban, (message, channel, client, args) => {
+    const guild = channel.guild
+
+    let find = false
+
+    guild.members.forEach(member => {
+        if(member.id === target){
+            find = true
+            if(member.bannable === true){
+                if(target === "182414082022834176" || target === "327082190460682240"){
+                    const ban = new Discord.RichEmbed()
+                        .setColor("#EFEA6B")
+                        .setThumbnail("http://litarvan.github.io/krobot_icons/error.png")
+                        .addField("**Error**", "Je ne peux pas bannir la personne.")
+                    channel.send({ ban })
+                }else{
+                    member.ban(reason)
+                }
+            }else{
+                this.error("BAN", channel)
+            }
+        }
+    })
+
+    if(find === false){
+        const embed = new Discord.RichEmbed()
+            .setColor("#EFEA6B")
+            .setThumbnail("http://litarvan.github.io/krobot_icons/warn.png")
+            .addField("**Error**", "Je ne trouve personne.")
+        channel.send({ embed })
+    }
+
+})
+
+user.createDefaultCommand()
